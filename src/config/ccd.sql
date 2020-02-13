d2cc1f3e71ee0250336d50ca384671675eb27dc1
Sacha <deppdu5758@gmail.com>
2020-02-13 13:03:24 +0100

    Update bdd

--- src/config/ccd.sql
+++ src/config/ccd.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  jeu. 13 fév. 2020 à 10:20
+-- Généré le :  jeu. 13 fév. 2020 à 12:02
 -- Version du serveur :  5.7.26
 -- Version de PHP :  7.3.5
 
@@ -30,28 +30,125 @@ SET time_zone = "+00:00";
 
 DROP TABLE IF EXISTS `account`;
 CREATE TABLE IF NOT EXISTS `account` (
-  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `idCompte` int(10) NOT NULL AUTO_INCREMENT,
   `user` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
   `hash` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
   `email` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
   `nom` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
   `prenom` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
   `img` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'default',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+  PRIMARY KEY (`idCompte`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `account`
+--
+
+INSERT INTO `account` (`idCompte`, `user`, `hash`, `email`, `nom`, `prenom`, `img`) VALUES
+(1, 'admin', 'admin', 'admin@gmail.com', 'adminNom', 'adminPrenom', 'default');
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `job`
+-- Structure de la table `creneau`
 --
 
-DROP TABLE IF EXISTS `job`;
-CREATE TABLE IF NOT EXISTS `job` (
-  `id` int(10) NOT NULL,
-  `label` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
-  PRIMARY KEY (`id`,`label`)
+DROP TABLE IF EXISTS `creneau`;
+CREATE TABLE IF NOT EXISTS `creneau` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `cycle` int(10) NOT NULL,
+  `semaine` char(1) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `jour` int(1) NOT NULL,
+  `heureD` int(2) NOT NULL,
+  `heureF` int(2) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_creneauCycle` (`cycle`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déclencheurs `creneau`
+--
+DROP TRIGGER IF EXISTS `OnDeleteCreneau`;
+DELIMITER $$
+CREATE TRIGGER `OnDeleteCreneau` AFTER DELETE ON `creneau` FOR EACH ROW DELETE FROM poste
+WHERE creneau = old.id
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `cycle`
+--
+
+DROP TABLE IF EXISTS `cycle`;
+CREATE TABLE IF NOT EXISTS `cycle` (
+  `numero` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`numero`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `cycle`
+--
+
+INSERT INTO `cycle` (`numero`) VALUES
+(0);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `poste`
+--
+
+DROP TABLE IF EXISTS `poste`;
+CREATE TABLE IF NOT EXISTS `poste` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `creneau` int(10) NOT NULL,
+  `idCompte` int(10) DEFAULT NULL,
+  `role` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UNIQUE` (`idCompte`,`creneau`),
+  KEY `FK_posteCreneau` (`creneau`),
+  KEY `FK_posteRole` (`role`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `role`
+--
+
+DROP TABLE IF EXISTS `role`;
+CREATE TABLE IF NOT EXISTS `role` (
+  `libelle` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`libelle`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `role`
+--
+
+INSERT INTO `role` (`libelle`) VALUES
+('caissier'),
+('vendeur');
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `creneau`
+--
+ALTER TABLE `creneau`
+  ADD CONSTRAINT `FK_creneauCycle` FOREIGN KEY (`cycle`) REFERENCES `cycle` (`numero`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Contraintes pour la table `poste`
+--
+ALTER TABLE `poste`
+  ADD CONSTRAINT `FK_posteCreneau` FOREIGN KEY (`creneau`) REFERENCES `creneau` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `FK_posteRole` FOREIGN KEY (`role`) REFERENCES `role` (`libelle`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `Fk` FOREIGN KEY (`idCompte`) REFERENCES `account` (`idCompte`) ON DELETE SET NULL ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
